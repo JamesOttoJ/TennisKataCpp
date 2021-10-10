a567cb9d39f0b329eaa0f2f43ce7187d90801ca5
JamesOttoJ <55111157+JamesOttoJ@users.noreply.github.com>
2021-10-10 13:03:41 -0500

    Add project files.

---
+++ PairingForGoodTest.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../TennisKata/TennisKata.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace PairingForGood;
+
+namespace PairingForGoodTest
+{
+	TEST_CLASS(PairingForGoodTest)
+	{
+	public:
+		
+		TEST_METHOD(ThreePlusThree)
+		{
+			int i = 3, j = 3;
+			int actual = i + j;
+			Assert::AreEqual(6, actual);
+		}
+	};
+}
